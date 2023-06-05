11034e21c8b6f644a436fc5151535a8fa45305cf
maruf <dev.maruf112@gmail.com>
2023-06-05 06:18:05 +0600

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: any, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Server connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Server connected successfully')
     app.listen(config.port, () => {
-      console.log(`Server is  listening on port ${config.port}`);
-    });
+      console.log(`Server is  listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Server failed to connect", error);
+    console.log('Server failed to connect', error)
   }
 }
-main();
+main()
