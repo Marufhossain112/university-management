b219ffb578e27e281fb02dd56390e2869ae68629
maruf <dev.maruf112@gmail.com>
2023-06-06 07:49:55 +0600

    error messages and success messages differantiated

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+// import usersRouter from './app/modules/users/users.route'
+import usersRoutes from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
+
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: any, res: Response) => {
-  res.send('Hello World!')
+// Application routes
+// console.log(app.get('env'))
+// console.log(process.env)
+
+app.use('/api/v1/users/', usersRoutes)
+//Testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
 export default app
